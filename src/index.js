016394f6da8fa9edec352bef193e61e854380f66
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2022-11-08 21:49:06 +0530

    strict mode removed, its create problem while testing in console

--- src/index.js
+++ src/index.js
@@ -10,9 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider>
   <BrowserRouter>
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
   </BrowserRouter>
   </ChakraProvider>
 );
