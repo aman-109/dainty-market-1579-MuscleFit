77f29ceb10c31e852440a66e3b076647bd5b0e20
SaurabhKumar <srbhkumar624@gmail.com>
2022-11-08 18:30:43 +0530

    added navabar to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <Footer/>
     </div>
   );
 }
