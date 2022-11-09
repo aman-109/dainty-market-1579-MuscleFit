47f069017dc5b0617656661df9aaaf9fa23284fd
aman-109 <aman.mate.52@gmail.com>
2022-11-09 22:05:17 +0530

    done

--- src/Pages/exercise/Exercise.jsx
+++ src/Pages/exercise/Exercise.jsx
@@ -1,5 +1,4 @@
-import Navbar from "../../components/Navbar"
-import Footer from "../../components/Footer"
+
 import Avatar from "./Avatar"
 import FilterTable from "./FilterTable"
 import { Box, Text } from "@chakra-ui/react"
@@ -9,13 +8,13 @@ export default function Exercise(){
 
     return (
         <Box>
-            <Navbar/>
+           
             <Text fontSize={{base:"20px",md:"24px", lg:"28px"}} marginTop="50px" marginBottom="20px">JEFIT Exercise Database</Text>
             <Text fontSize={{base:"16px",md:"20px", lg:"24px"}}>Exercises By Muscle Group</Text>
             <Text fontSize={{base:"14px",md:"18px", lg:"22px"}} marginBottom="30px" color="rgb(131, 136, 143)">Find exercises that work out the muscle groups you're targeting.</Text>
             <Avatar/>
             <FilterTable/>
-            <Footer/>
+      
         </Box>
     )
 }
