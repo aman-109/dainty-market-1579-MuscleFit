73c51284df84637ff25db8721bd3659e47755af0
nilamkhose2001 <nilamkhose2001@gmail.com>
2022-11-09 22:54:44 +0530

    add

--- src/components/Tracking.jsx
+++ src/components/Tracking.jsx
@@ -1,5 +1,5 @@
-import { Box, Container,Flex,Text,Image } from "@chakra-ui/react"
-
+import { Box, Container,Flex,Text,Image, Button } from "@chakra-ui/react"
+import {AiFillCheckCircle} from "react-icons/ai"
 
 export const Tracking=()=>{
     return <>
@@ -14,18 +14,18 @@ export const Tracking=()=>{
                 >JEFIT COVERS ALL OF YOUR WORKOUT NEEDS</Text>
 
 
-<Flex  direction={{ base: 'column', md: 'column',lg:'row' }} w='100%' mt={6}>
-<Box  w={{ base: '100%', md: '90%', lg: '50%' ,xl:'50%' ,'2xl':'50%'}} mt={12} ml={6}>
+<Flex  direction={{ base: 'column', md: 'column',lg:'row' }} w='100%' mt={6} gap={6}>
+<Box  w={{ base: '100%', md: '90%', lg: '50%' ,xl:'50%' ,'2xl':'50%'}} mt={12} ml={6} >
     <Image src='https://www.jefit.com/wp/wp-content/uploads/2022/08/web1-1536x1536.png' alt='tracking' />
 </Box>
-<Box  w={{ base: '100%', md: '100%', lg: '50%' ,xl:'50%' ,'2xl':'50%' }}  mr='0px' mt={10} ml={6} >
+<Box  w={{ base: '100%', md: '100%', lg: '40%' ,xl:'40%' ,'2xl':'50%' }}  mr='0px' mt={10} ml={6}  >
     <Text 
     fontFamily='Barlow Semi Condensed, sans-serif'
     color='#8BC9DE'
     fontSize='15px'
     fontWeight='400'
     lineHeight='20px'
-    mt={12}
+    mt={16}
     textAlign='left'
     >
         WORKOUT TRACKING
@@ -41,6 +41,54 @@ export const Tracking=()=>{
     >
     PERSONALIZE YOUR <br /> WORKOUT PLANS
     </Text>
+    <Box mt={10} >
+       <Flex gap={3}>
+       <AiFillCheckCircle style={{color:"#2C94FE",marginTop:"3%"}} />
+        <Text
+        fontFamily='"DM Sans", sans-serif'
+        fontSize='17px'
+        fontWeight='500'
+        lineHeight='40px'
+        >
+        1400+ exercises with instructions
+        </Text>
+       </Flex>
+       <Flex gap={3}>
+       <AiFillCheckCircle style={{color:"#2C94FE",marginTop:"3%"}} />
+        <Text
+        fontFamily='"DM Sans", sans-serif'
+        fontSize='17px'
+        fontWeight='500'
+        lineHeight='40px'
+        >
+        Pro-designed workout plans
+        </Text>
+       </Flex>
+       <Flex gap={3}>
+       <AiFillCheckCircle style={{color:"#2C94FE",marginTop:"3%"}} />
+        <Text
+        fontFamily='"DM Sans", sans-serif'
+        fontSize='17px'
+        fontWeight='500'
+        lineHeight='40px'
+        >
+        Support custom exercises
+        </Text>
+       </Flex>
+       <Flex gap={3}>
+       <AiFillCheckCircle style={{color:"#2C94FE",marginTop:"3%"}} />
+        <Text
+        fontFamily='"DM Sans", sans-serif'
+        fontSize='17px'
+        fontWeight='500'
+        lineHeight='40px'
+        >
+        Flexible workout planning tool
+        </Text>
+       </Flex>
+       <Button  variant='solid' bg='#39B7FF' color='white' fontFamily='Montserrat, sans-serif' fontSize='20px' fontWight='700' style={{borderRadius:'20px',padding:'1% 10%'}}>JOIN NOW</Button>
+        
+    </Box>
 </Box>
     </Flex>
 
