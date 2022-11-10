e829e39bcc6267630ecc18f7049972e794934332
soumitra-dey <soumitra.petbindhi@gmail.com>
2022-11-10 21:40:48 +0530

    soumitra-day-3

---
+++ src/Pages/exercise/CreateExcercise.jsx
@@ -0,0 +1,33 @@
+import { Box, Button, Grid, Image, Text } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
+import data from "./data.json"
+
+
+
+export default function CreateExercise(){
+    // console.log(data[1])
+
+    return (
+        <Box>
+            <Text fontSize="24px" paddingTop="20px">Try Something New</Text>
+            <Text fontSize="20px" paddingTop="10px" color="rgb(88, 95, 105)" paddingBottom="20px">Change up your routine with one of these exercises!</Text>
+            <Grid templateColumns={{base:"repeat(2,1fr)", md:"repeat(4,1fr)", lg:"repeat(6,1fr)"}} w={{base:"90%", md:"80%", lg:"80%"}} margin="auto" gap="15px">
+                {
+                    data.map((el)=>(
+                        <Box key={el.id} w="100%">
+                            <Link>
+                                <Image src={el.image1} w="100%"/>
+                                <Text fontSize="14px" margin="0px" color="rgb(51, 181, 229)" fontWeight="500">{el.title}</Text>
+                                <Text fontSize="14px" margin="0px">{el.main_group},{el.detailed_group}</Text>
+                            </Link>
+                        </Box>
+                    ))
+                }
+            </Grid>
+            <Box marginTop="20px" marginBottom="20px">
+                <Button fontSize="12px" h="35px" backgroundColor="rgb(33, 144, 199)" color="white" _hover={{backgroundColor:"rgb(33, 110, 199)"}}>Create Custom Exercise</Button>
+            </Box>
+        </Box>
+    )
+}
+
--- src/Pages/exercise/Exercise.jsx
+++ src/Pages/exercise/Exercise.jsx
@@ -2,19 +2,19 @@
 import Avatar from "./Avatar"
 import FilterTable from "./FilterTable"
 import { Box, Text } from "@chakra-ui/react"
+import CreateExercise from "./CreateExcercise"
 
 
 export default function Exercise(){
 
     return (
-        <Box>
-           
+        <Box backgroundColor="rgb(252, 252, 252)">
             <Text fontSize={{base:"20px",md:"24px", lg:"28px"}} marginTop="50px" marginBottom="20px">JEFIT Exercise Database</Text>
             <Text fontSize={{base:"16px",md:"20px", lg:"24px"}}>Exercises By Muscle Group</Text>
             <Text fontSize={{base:"14px",md:"18px", lg:"22px"}} marginBottom="30px" color="rgb(131, 136, 143)">Find exercises that work out the muscle groups you're targeting.</Text>
             <Avatar/>
             <FilterTable/>
-      
+            <CreateExercise/>
         </Box>
     )
 }
