acc21e981d1ea9c4346b575c15916aeaa35b2865
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2022-11-12 00:29:42 +0530

    crouser fixed

--- src/components/BoxCrouser.js
+++ src/components/BoxCrouser.js
@@ -11,10 +11,11 @@ const BoxCrouser = () => {
     const navigate = useNavigate();
     const hstyle = {
         display:"flex",
-        overflowX:"scroll",
+        overflowX:"hidden",
         scrollBehavior:"smooth",
+        borderRadius:"15px",
         
-        width:"90%",
+        width:"80%",
         height:"auto",
         margin:"auto",
         position:"relative",
@@ -25,28 +26,29 @@ const BoxCrouser = () => {
        const boxRef = useRef()
        let box = boxRef.current 
 
+       const paraMeter = !smallScreen?"1600":"683"
 
     function doIt(){
-        if(box.scrollLeft<"300"){
-            box.scrollLeft = 1800
+        if(box.scrollLeft<"170"){
+            box.scrollLeft = 870
         } else{
-            box.scrollLeft = box.scrollLeft - 300
+            box.scrollLeft = box.scrollLeft - 170
         }
     }
     function doIt2(){
-        if(box.scrollLeft>"1700"){
-            box.scrollLeft = 10
+        if(box.scrollLeft>paraMeter){
+            box.scrollLeft = 0
         } else {
-            box.scrollLeft = box.scrollLeft + 300
+            box.scrollLeft = box.scrollLeft + 170
         }
-        /* console.log(box.scrollLeft) */
+        console.log(box.scrollLeft)
     }
 
     function runIt(){
         if(varForCrouser){
         setInterval(() => {
                 doIt2()
-        }, 3500);
+        }, 1500);
         varForCrouser = false
     }
     }
@@ -77,8 +79,8 @@ const BoxCrouser = () => {
 
   return (
     <Box mt={16}>
-        {/* <Button onClick={doIt} >left</Button>
-        <Button onClick={doIt2}>right</Button> */}
+        <Button onClick={doIt} >left</Button>
+        <Button onClick={doIt2}>right</Button>
                 <Heading as='h3' size='xl' color='#2F373D'>CONNECT WITH OVER 10 MILLION JEFIT MEMBERS</Heading>
 
                 <Text 
@@ -94,47 +96,47 @@ const BoxCrouser = () => {
         >As the most active workout community, we invite you and your friends to join us and support each other.</Text>
 
         <Box id="hello" onMouseEnter={runIt}  ref={boxRef} style={hstyle}>
-            <Box className="look"  w='10%' style={{border:'1px solid green'}} >
-                <Image src="https://www.jefit.com/wp/wp-content/uploads/2021/11/15members-150x150.png.webp" alt="1"></Image>
+            <Box className="look" style={{width:"20%",height:"100%" }} >
+                <Image  borderRadius="8px" src="https://www.jefit.com/wp/wp-content/uploads/2021/11/15members-150x150.png.webp" alt="1"></Image>
             </Box>
-            <Box className="look" style={{width:"20%",height:"100%",border:"1px solid red", }} >
-                <Image src="https://www.jefit.com/wp/wp-content/uploads/2021/11/20members-150x150.png.webp" alt="2"></Image>
+            <Box className="look" style={{width:"20%",height:"100%" }} >
+                <Image  borderRadius="8px" src="https://www.jefit.com/wp/wp-content/uploads/2021/11/20members-150x150.png.webp" alt="2"></Image>
             </Box>
-            <Box className="look" style={{width:"20%",height:"100%",border:"1px solid red", }} >
-                <Image src="https://www.jefit.com/wp/wp-content/uploads/2021/11/7members-150x150.png.webp" alt="3"></Image>
+            <Box className="look" style={{width:"20%",height:"100%" }} >
+                <Image  borderRadius="8px" src="https://www.jefit.com/wp/wp-content/uploads/2021/11/7members-150x150.png.webp" alt="3"></Image>
             </Box>
-            <Box className="look" style={{width:"20%",height:"100%",border:"1px solid red", }} >
-                <Image src="https://www.jefit.com/wp/wp-content/uploads/2021/11/12members-150x150.png.webp" alt="4"></Image>
+            <Box className="look" style={{width:"20%",height:"100%" }} >
+                <Image  borderRadius="8px" src="https://www.jefit.com/wp/wp-content/uploads/2021/11/12members-150x150.png.webp" alt="4"></Image>
             </Box>
-            <Box className="look" style={{width:"20%",height:"100%",border:"1px solid red", }} >
-                 <Image src="https://www.jefit.com/wp/wp-content/uploads/2021/11/18members-150x150.png.webp" alt="5"></Image>
+            <Box className="look" style={{width:"20%",height:"100%" }} >
+                 <Image borderRadius="8px"  src="https://www.jefit.com/wp/wp-content/uploads/2021/11/18members-150x150.png.webp" alt="5"></Image>
 
             </Box>
-            <Box className="look" style={{width:"20%",height:"100%",border:"1px solid red", }} >
-                 <Image src="https://www.jefit.com/wp/wp-content/uploads/2021/11/8members-150x150.png.webp" alt="6"></Image>
+            <Box className="look" style={{width:"20%",height:"100%" }} >
+                 <Image borderRadius="8px"  src="https://www.jefit.com/wp/wp-content/uploads/2021/11/8members-150x150.png.webp" alt="6"></Image>
 
             </Box>
-            <Box className="look" style={{width:"20%",height:"100%",border:"1px solid red", }} >
-                   <Image src="https://www.jefit.com/wp/wp-content/uploads/2021/11/19members-150x150.png.webp" alt="7"></Image>
+            <Box className="look" style={{width:"20%",height:"100%" }} >
+                   <Image borderRadius="8px" ge src="https://www.jefit.com/wp/wp-content/uploads/2021/11/19members-150x150.png.webp" alt="7"></Image>
 
             </Box>
-            <Box className="look" style={{width:"20%",height:"100%",border:"1px solid red", }} >
-                <Image src="https://www.jefit.com/wp/wp-content/uploads/2021/11/15members-150x150.png.webp" alt="8"></Image>
+            <Box className="look" style={{width:"20%",height:"100%" }} >
+                <Image  borderRadius="8px" src="https://www.jefit.com/wp/wp-content/uploads/2021/11/15members-150x150.png.webp" alt="8"></Image>
 
             </Box>
-            <Box className="look" style={{width:"20%",height:"100%",border:"1px solid red", }} >
-                <Image src="https://www.jefit.com/wp/wp-content/uploads/2021/11/20members-150x150.png.webp" alt="9"></Image>
+            <Box className="look" style={{width:"20%",height:"100%" }} >
+                <Image  borderRadius="8px" src="https://www.jefit.com/wp/wp-content/uploads/2021/11/20members-150x150.png.webp" alt="9"></Image>
 
             </Box>
-            <Box className="look" style={{width:"20%",height:"100%",border:"1px solid red", }} >
-                 <Image src="https://www.jefit.com/wp/wp-content/uploads/2021/11/7members-150x150.png.webp" alt="10"></Image>
+            <Box className="look" style={{width:"20%",height:"100%" }} >
+                 <Image borderRadius="8px"  src="https://www.jefit.com/wp/wp-content/uploads/2021/11/7members-150x150.png.webp" alt="10"></Image>
 
             </Box>
-            <Box className="look" style={{width:"20%",height:"100%",border:"1px solid red", }} >
-                <Image src="https://www.jefit.com/wp/wp-content/uploads/2021/11/12members-150x150.png.webp" alt="4"></Image>
+            <Box className="look" style={{width:"20%",height:"100%" }} >
+                <Image  borderRadius="8px" src="https://www.jefit.com/wp/wp-content/uploads/2021/11/12members-150x150.png.webp" alt="4"></Image>
             </Box>
-            <Box className="look" style={{width:"20%",height:"100%",border:"1px solid red", }} >
-                 <Image src="https://www.jefit.com/wp/wp-content/uploads/2021/11/18members-150x150.png.webp" alt="5"></Image>
+            <Box className="look" style={{width:"20%",height:"100%" }} >
+                 <Image borderRadius="8px"  src="https://www.jefit.com/wp/wp-content/uploads/2021/11/18members-150x150.png.webp" alt="5"></Image>
 
             </Box>
         </Box>
